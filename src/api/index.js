46b4fa85d9255eacb6296596c7403d5c3e082854
rantika <rantika97@gmail.com>
2021-03-14 15:20:15 +0700

    delete config setup proxy

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 // const access_token = localStorage.getItem("token"); // to get the token
 
 const API = Axios.create({
-  baseURL: `/api`,
+  baseURL: `https://lekturapp.herokuapp.com`,
 });
 
 export default API;
